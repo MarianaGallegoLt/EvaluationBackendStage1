5d9900522dc6e1ee120f0670b2a829600b1f2932
MarianaGallegoLt <103234941+MarianaGallegoLt@users.noreply.github.com>
2022-04-18 09:20:39 -0500

    program.cs added

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace BackendStage1Evaluation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+
+            Console.WriteLine("Ingrese un texto:");
+            text = Console.ReadLine();
+
+            Console.WriteLine(stringVerification(text));
+        }
+
+        static string stringVerification(string txt)
+        {
+            string texto = txt;
+            Boolean isEmpty = true;
+            string newString = "NA";
+
+            string[] invalidChars = new string[] { "@", "!", "\"", "#", "$", "%", "&", "/", "(", ")", "=", "?", "¿", "¡", "´", "¨", "+", "*", "~", "{", "}", "^", "[", "]", "`", ".", ";", ":", "," };
+            string[] validChars = new string[] { " ", "-", "_"};
+
+            foreach(char l in texto)
+            {
+                string lTxt = l.ToString();
+                if(Char.IsLetter(l) | char.IsDigit(l) | validChars.Contains(l.ToString()))
+                    isEmpty = false;
+            }
+
+            if (!isEmpty)
+            {
+                foreach (var c in invalidChars)
+                {
+                    texto = texto.Replace(c, string.Empty);
+                    newString = texto;
+                }
+            }
+            return newString;
+        }
+    }
+}
